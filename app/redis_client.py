7602c75cc0cd8155ae0cfe6f35fda1fbe40fcb1c
Samarthkumbar <samarthkumbar10@gmail.com>
2025-07-28 02:23:35 +0530

    redis change

--- app/redis_client.py
+++ app/redis_client.py
@@ -1,4 +1,7 @@
 # app/redis_client.py
 import redis
+import os
 
-redis_client = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
+redis_url = os.environ.get("REDIS_URL") 
+redis_client = redis.from_url(redis_url, decode_responses=True)
+
